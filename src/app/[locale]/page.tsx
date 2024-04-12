69e3ab81545e1cb013530707009fcff7c3529bac
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-11 21:16:06 -0400

    Start preparations for responsivity

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,15 +4,19 @@ import { useTranslations } from "next-intl";
 import { Header } from "../../components/Header";
 import { Banner } from "../../components/Banner";
 import { TopSellers } from "../../components/TopSellers";
+import { Footer } from "../../components/Footer";
 
 export default function Home() {
   const t = useTranslations();
 
   return (
-    <div className="flex min-h-screen max-w-full overflow-hidden flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen max-w-full overflow-hidden flex-col items-center justify-between pt-24">
       <Header />
-      <Banner />
+      {/* <Banner />
+
       <TopSellers />
+
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { useTranslations } from "next-intl";
+import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+
+export const Footer: React.FC = () => {
+  const t = useTranslations("footer");
+
+  const topTeamsOptions = [
+    t("topTeamsMapleLeafs"),
+    t("topTeamsOilers"),
+    t("topTeamsPenguins"),
+  ];
+  const supportOptions = [
+    t("supportHelp"),
+    t("supportContact"),
+    t("supportFAQ"),
+  ];
+
+  return (
+    <div className="w-full flex items-start flex-col mt-20">
+      <div className="w-full bg-black px-20 pt-6 pb-16">
+        <div className="flex-row flex justify-between items-start">
+          <div className="flex flex-col justify-start items-start w-1/5">
+            <h1 className="text-white font-semibold text-2xl">
+              {t("newsLetter")}
+            </h1>
+
+            <h1 className="text-white text-xs mt-3">{t("newsLetterText")}</h1>
+
+            <button className="py-2 px-4 rounded-sm bg-white font-semibold hover:bg-gray-200 mt-8">
+              {t("newsLetterButton")}
+            </button>
+          </div>
+
+          <div className="flex flex-col justify-start items-start w-1/4">
+            <h1 className="text-white font-semibold text-2xl mb-4">
+              {t("topTeamsTitle")}
+            </h1>
+
+            {topTeamsOptions.map((team, teamIndex) => (
+              <button
+                key={`${teamIndex} - ${team}`}
+                className="w-full text-left font-light text-white hover:text-gray-300 hover:font-normal"
+              >
+                {team}
+              </button>
+            ))}
+          </div>
+
+          <div className="flex flex-col justify-start items-start w-1/4">
+            <h1 className="text-white font-semibold text-2xl mb-4">
+              {t("supportTitle")}
+            </h1>
+
+            {supportOptions.map((option, optionIndex) => (
+              <button
+                key={`${optionIndex} - ${option}`}
+                className="w-full text-left font-light text-white hover:text-gray-300 hover:font-normal"
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+
+          <div className="flex flex-col justify-start items-start w-1/4">
+            <div className="w-full flex flex-row justify-start items-start gap-4">
+              <FaInstagram color="#ffff" size={24} className="cursor-pointer" />
+              <FaFacebook color="#ffff" size={24} className="cursor-pointer" />
+              <FaTwitter color="#ffff" size={24} className="cursor-pointer" />
+            </div>
+
+            <h1 className="text-slate-300 mt-4 font-light text-xs">
+              Aliquip nisi cupidatat occaecat irure proident et cupidatat veniam
+              veniam. Aliqua magna voluptate elit laborum reprehenderit irure
+              minim dolore ipsum quis ad. Excepteur exercitation fugiat magna
+              proident minim nulla amet irure labore cupidatat dolore sint magna
+              dolor.
+            </h1>
+          </div>
+        </div>
+
+        <div className="w-full border-t border-gray-200 mt-14 flex flex-row items-center justify-between pt-4">
+          <h1 className="text-white font-light text-xs">
+            Copyright 2024 | Powered by Bafica
+          </h1>
+
+          <h1 className="text-white font-light text-xs cursor-pointer hover:text-gray-200">
+            {t("privacyPolicy")}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
